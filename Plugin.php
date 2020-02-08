b295be7f42ecfa3920a1ad98a0044f915fb7a3c5
Benno <org.waldhauer@gmail.com>
2020-02-08 08:12:38 +0100

    initial commit

---
+++ Plugin.php
@@ -0,0 +1,40 @@
+<?php
+namespace Kanboard\Plugin\WeKanboard;
+use Kanboard\Core\Plugin\Base;
+class Plugin extends Base
+{
+    public function initialize()
+    {
+        $this->hook->on('template:layout:css', array('template' => 'plugins/WeKanboard/WeKanboard.css'));
+    }
+
+    public function getPluginName()
+    {
+        return 'WeKanboard';
+    }
+
+    public function getPluginDescription()
+    {
+        return t('Yet another theme for Kanboard. Strongly inspired by the style of Wekan.');
+    }
+
+    public function getPluginAuthor()
+    {
+        return 'Benno Waldhauer';
+    }
+
+    public function getPluginVersion()
+    {
+        return '1.0.0';
+    }
+
+    public function getPluginHomepage()
+    {
+        return 'https://github.com/bw-hro/WeKanboard';
+    }
+
+    public function getCompatibleVersion()
+    {
+        return '>=1.2.13';
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# WeKanboard
+![WeKanboard-sample](https://user-images.githubusercontent.com/1141914/74080997-2363cd80-4a4a-11ea-94b6-fae981f1454b.png)
